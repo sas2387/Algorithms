c533fecd9b657d054f273472dfebaf679b10e977
Siddharth Shah <sas2387@columbia.edu>
2016-11-14 21:04:45 -0500

    Problem 226

---
+++ LeetCode/src/InvertTree.java
@@ -0,0 +1,30 @@
+/**
+ * LeetCode
+ * Problem 226
+ * @author siddharthshah
+ *
+ */
+public class InvertTree {
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	public TreeNode invertTree(TreeNode root) {
+		if (root == null) {
+			return null;
+		}
+		TreeNode right = invertTree(root.right);
+		TreeNode left = invertTree(root.left);
+		root.left = right;
+		root.right = left;
+		return root;
+	}
+
+}
