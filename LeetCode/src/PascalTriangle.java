86ede6477081bc2798d48ab041f2a710b5aedc4e
Siddharth Shah <sas2387@columbia.edu>
2016-11-19 17:16:34 -0500

    Problem 118

---
+++ LeetCode/src/PascalTriangle.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * LeetCode
+ * Problem 118
+ * @author Siddharth
+ *
+ */
+public class PascalTriangle {
+
+	public static void main(String[] args) {
+		PascalTriangle pt = new PascalTriangle();
+		System.out.println(pt.generate(5));
+	}
+
+	public List<List<Integer>> generate(int numRows) {
+		List<List<Integer>> numbers = new ArrayList<>();
+		List<Integer> prevList = null;
+		int row = 1;
+		while (row <= numRows) {
+			List<Integer> rowNumbers = new ArrayList<>();
+			if (row == 1) {
+				rowNumbers.add(1);
+				prevList = rowNumbers;
+				numbers.add(rowNumbers);
+			} else {
+				rowNumbers.add(1);
+				int currIndex = 1;
+				int newNumber;
+				while (currIndex < prevList.size()) {
+					newNumber = prevList.get(currIndex - 1) + prevList.get(currIndex);
+					rowNumbers.add(newNumber);
+					currIndex++;
+				}
+				rowNumbers.add(1);
+				prevList = rowNumbers;
+				numbers.add(rowNumbers);
+			}
+			row++;
+		}
+		return numbers;
+	}
+}
